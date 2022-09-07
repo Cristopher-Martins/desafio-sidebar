783d77f2ac5a301ca98038ce41b01010f8538d90
Cristopher Martins <cristopher_martins@hotmail.com>
2022-09-07 04:45:56 -0300

    fixed indentation | add classes to img on logo and footer | add content to styles.css | add content to script.js

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+const getAside = document.querySelector('aside')
+const getAsideFooter = document.querySelector('.aside-footer')
+const getP = document.querySelector('p')
+
 function clicou () {
-    console.log("clicou")
+    const opened = getAside.classList.contains('opened')
+    const closed = getAside.classList.contains('closed')
+    
+
+    if (closed == true) {    
+        getAside.classList.remove('closed')
+        getAside.classList.add('opened')
+        getAsideFooter.classList.remove('closed')
+        getAsideFooter.classList.add('opened')
+        getP.style.display = "block";
+    }
+    else if (opened == true) {        
+        getAside.classList.remove('opened')
+        getAside.classList.add('closed')
+        getAsideFooter.classList.remove('opened')
+        getAsideFooter.classList.add('closed')
+        getP.style.display = "block";
+    }
+    else {
+        getAside.classList.add('opened')
+        getAsideFooter.classList.add('opened')
+        getP.style.display = "block";
+    }
 }
