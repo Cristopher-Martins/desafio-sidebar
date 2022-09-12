2f9d8def4eebcaee34d95da3a82509e5952144a5
Cristopher Martins <cristopher_martins@hotmail.com>
2022-09-12 20:36:54 -0300

    add fadeIN effect to sidebar content

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const getP = document.querySelectorAll('p')
 const getAsideLogo = document.querySelector('.aside-logo')
 const getAvatar = document.querySelector('.avatar')
 
+
+ 
 function clicou () {
     const opened = getAside.classList.contains('opened')
     const closed = getAside.classList.contains('closed')
@@ -11,26 +13,45 @@ function clicou () {
     if (closed) {    
         getAside.classList.remove('closed')
         getAside.classList.add('opened')
-        getP.forEach(e => {
-            e.style.display = "block";
-        });
+        getP.forEach(p =>{            
+            p.classList.remove('fadeOut')
+            p.classList.add('fadeIn')
+            p.classList.add('opened')
+            p.style.display = "block";
+        })       
+
+        getAsideLogo.classList.remove('fadeOut');
+        getAsideLogo.classList.add('fadeIn');
+        getAvatar.classList.remove('fadeOut');        
+        getAvatar.classList.add('fadeIn');
         getAsideLogo.style.display = "block";
         getAvatar.style.display = "block";
     }
     else if (opened) {        
         getAside.classList.remove('opened')
-        getAside.classList.add('closed')
-        getP.forEach(e => {
-            e.style.display = "none";
-        });
-        getAsideLogo.style.display = "none";
-        getAvatar.style.display = "none";
+        getAside.classList.add('closed');
+        getP.forEach(p =>{
+            p.classList.remove('fadeIn')
+            p.classList.add('fadeOut')
+        })
+        setTimeout(() => getP.forEach(p => {
+            p.style.display = "none";
+        }), 500)
+        
+        getAsideLogo.classList.remove('fadeIn');
+        getAsideLogo.classList.add('fadeOut');
+        getAvatar.classList.remove('fadeIn');        
+        getAvatar.classList.add('fadeOut');
+        
+        setTimeout( () => { (getAsideLogo.style.display = "none")
+        }, 500);
+        setTimeout( () => { (getAvatar.style.display = "none")
+        }, 500);
     }
     else {
-        console.log(getAsideLogo ,getAvatar)
-
         getAside.classList.add('opened')
         getP.forEach(e => {
+            e.classList.add('opened')
             e.style.display = "block";
         });    
         getAsideLogo.style.display = "block";
