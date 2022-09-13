3d0e3f3665948211b44526209826aca8a8672826
Cristopher Martins <cristopher_martins@hotmail.com>
2022-09-13 02:10:34 -0300

    change function name, add user content to a div | change .add and .remove methods to .replace method | create translate animation to fix issue on logout button

--- script.js
+++ script.js
@@ -2,47 +2,43 @@ const getAside = document.querySelector('aside')
 const getP = document.querySelectorAll('p')
 const getAsideLogo = document.querySelector('.aside-logo')
 const getAvatar = document.querySelector('.avatar')
-
-
+const getButtonTranslate = document.getElementById('translate-button')
  
-function clicou () {
+function sidebar () {
     const opened = getAside.classList.contains('opened')
-    const closed = getAside.classList.contains('closed')
-    
+    const closed = getAside.classList.contains('closed')  
 
     if (closed) {    
-        getAside.classList.remove('closed')
-        getAside.classList.add('opened')
+        getAside.classList.replace('closed','opened')
         getP.forEach(p =>{            
-            p.classList.remove('fadeOut')
-            p.classList.add('fadeIn')
+            p.classList.replace('fadeOut','fadeIn')
             p.classList.add('opened')
             p.style.display = "block";
-        })       
-
-        getAsideLogo.classList.remove('fadeOut');
-        getAsideLogo.classList.add('fadeIn');
-        getAvatar.classList.remove('fadeOut');        
-        getAvatar.classList.add('fadeIn');
+        })
+        // adicionando o efeito translate para o botão de logout e mudando caso já o tenha. 
+        if(getButtonTranslate.classList.contains('translateOut')){
+            getButtonTranslate.classList.replace('translateOut','translateIn')
+        }
+        else{
+            getButtonTranslate.classList.add('translateIn')
+        }            
+        getAsideLogo.classList.replace('fadeOut','fadeIn');
+        getAvatar.classList.replace('fadeOut','fadeIn');        
         getAsideLogo.style.display = "block";
         getAvatar.style.display = "block";
     }
     else if (opened) {        
-        getAside.classList.remove('opened')
-        getAside.classList.add('closed');
+        getAside.classList.replace('opened','closed')
         getP.forEach(p =>{
-            p.classList.remove('fadeIn')
-            p.classList.add('fadeOut')
+            p.classList.replace('fadeIn','fadeOut')
         })
         setTimeout(() => getP.forEach(p => {
             p.style.display = "none";
         }), 500)
-        
-        getAsideLogo.classList.remove('fadeIn');
-        getAsideLogo.classList.add('fadeOut');
-        getAvatar.classList.remove('fadeIn');        
-        getAvatar.classList.add('fadeOut');
-        
+        getButtonTranslate.classList.replace('translateIn','translateOut')
+        getAsideLogo.classList.replace('fadeIn','fadeOut');
+        getAvatar.classList.replace('fadeIn','fadeOut');
+        // timeout para dar tempo à animação, logo em seguida os elementos não são mais visiveis
         setTimeout( () => { (getAsideLogo.style.display = "none")
         }, 500);
         setTimeout( () => { (getAvatar.style.display = "none")
